f870ffdc842a88d52ec88f9e72db9a51b17cf079
igor.gutnik <igor.gutnik@e67b0f8a-f93e-11de-ba51-6b4196a2b81e>
2010-05-29 21:17:01 +0000

    Continue to work on regression tests

---
+++ unittest/regression/cmd_line.cpp
@@ -0,0 +1,123 @@
+#include <stdexcept>
+#include <vector>
+#include "unittest/regression/cmd_line.hpp"
+#include "unittest/thirdparty/tclap/CmdLine.h"
+
+
+cmd_line_c::cmd_line_c (int argc, char* argv [])
+  : m_dry_run  (false),
+    m_run_test (true),
+    m_inspect  (false)
+{
+  using namespace TCLAP;
+  CmdLine cmd ("BeaEngine regression test utility", ' ', "0.1");
+
+  SwitchArg a_dry_run  ("d", "dry_run" , "Dry run only.", false);
+  SwitchArg a_run_test ("r", "run_test", "Run test and update results", false);
+  SwitchArg a_inspect  ("s", "inspect" , "Inspect results", false); 
+
+  std::vector <Arg*> fv;
+  fv.push_back (&a_dry_run);
+  fv.push_back (&a_run_test);
+  fv.push_back (&a_inspect);
+  cmd.xorAdd (fv);
+
+  ValueArg <std::string> a_result ("o", "output", "Zip results data base", false, "", "filename", cmd);
+  ValueArg <std::string> a_table  ("i", "input", "Input file", false, "", "filename", cmd);
+  ValueArg <std::string> a_name   ("n", "name",  "Name of this test", false, "", "string", cmd);
+  ValueArg <std::string> a_comment ("m", "comment",  "Comment for this test", false, "", "string", cmd);
+
+  cmd.parse( argc, argv );
+  
+  if (a_dry_run.isSet ())
+    {
+      m_dry_run  = true;
+      m_inspect  = false;
+      m_run_test = false;
+    }
+  if (a_inspect.isSet ())
+    {
+      m_dry_run  = false;
+      m_inspect  = true;
+      m_run_test = false;
+    }
+
+  if (m_dry_run)
+    {
+      if (a_result.isSet ())
+	{
+	  throw std::runtime_error ("Zip results data base is meaningless for dry run");
+	}
+      if (a_name.isSet ())
+	{
+	  throw std::runtime_error ("Test name is meaningless for dry run");
+	}
+      if (a_comment.isSet ())
+	{
+	  throw std::runtime_error ("Test comment is meaningless for dry run");
+	}
+      if (!a_table.isSet ())
+	{
+	  throw std::runtime_error ("Table is required for dry run");
+	}
+      m_table_file = a_table.getValue ();
+      return;
+    }
+  if (m_run_test)
+    {
+      if (!a_result.isSet ())
+	{
+	  throw std::runtime_error ("Zip results data base is required");
+	}
+      if (!a_name.isSet ())
+	{
+	  throw std::runtime_error ("Test name is required");
+	}
+      if (!a_comment.isSet ())
+	{
+	  throw std::runtime_error ("Test comment is required");
+	}
+      if (!a_table.isSet ())
+	{
+	  throw std::runtime_error ("Table is required for dry run");
+	}
+      m_result_file = a_result.getValue ();
+      m_name        = a_name.getValue ();
+      m_comment     = a_comment.getValue ();
+      m_table_file  = a_table.getValue ();
+      return;
+    }
+  
+}
+// -----------------------------------------------------
+// in case of dry run, only table_file is mandatory
+bool cmd_line_c::dry_run () const
+{
+  return m_dry_run;
+}
+// -----------------------------------------------------
+std::string cmd_line_c::result_file () const
+{
+  return m_result_file;
+}
+// -----------------------------------------------------
+bool cmd_line_c::run_test () const
+{
+  return m_run_test;
+}
+// -----------------------------------------------------
+std::string cmd_line_c::name () const
+{
+  return m_name;
+}
+// -----------------------------------------------------
+std::string cmd_line_c::comment () const
+{
+  return m_comment;
+}
+// -----------------------------------------------------
+std::string cmd_line_c::table_file () const
+{
+  return m_table_file;
+}
+// -----------------------------------------------------
--- unittest/regression/main.cpp
+++ unittest/regression/main.cpp
@@ -1,8 +1,17 @@
+#include <fstream>
 #include <iostream>
 #include <iomanip>
 #include <stdexcept>
 #include <list>
 #include <string.h>
+#include <stdio.h>
+
+#if !defined (WIN32)
+#include <unistd.h>
+#else
+#include <io.h>
+#define _unlink unlink
+#endif
 
 #include <beaengine/BeaEngine.h>
 
@@ -11,13 +20,72 @@
 #include "unittest/regression/dasm_result.hpp"
 #include "unittest/regression/Timer.h"
 #include "unittest/regression/zip_archive.hpp"
-// ===================================================================
-static void do_disasm_test (const char* table_file, dasm_result_c& result)
+#include "unittest/regression/cmd_line.hpp"
+#include "unittest/regression/temp_path.hpp"
+
+
+static bool file_exists (const std::string& path)
 {
-  std::cout << "Loading table from " << table_file << std::endl;
-  table_reader_c reader (table_file);
-  std::cout << "Table loaded. Number of entries: " 
-	    << reader.items () << std::endl;
+  FILE* f = fopen (path.c_str (), "r");
+  if (f)
+    {
+      fclose (f);
+      return true;
+    }
+  return false;
+}
+// ------------------------------------------------------------
+
+static bool copy_file (const std::string& src, const std::string& dst)
+{
+  FILE* fi = fopen (src.c_str (), "rb");
+  FILE* fo = fopen (dst.c_str (), "wb");
+  const unsigned int BUFSZ = 1024*1024;
+
+  char   *buf;
+  size_t amount;
+  size_t written;
+  bool   result;
+
+  buf = new char[BUFSZ];
+
+  result = true;
+  if  ((fi == NULL) || (fo == NULL) )
+    {
+      result = false;
+      if (fi != NULL) fclose(fi);
+      if (fo != NULL) fclose(fo);
+    }
+  
+  if (result)
+    {
+      do
+	{
+	  amount = fread( buf, sizeof(char), BUFSZ, fi );
+	  if (amount)
+	    {
+	      written = fwrite( buf, sizeof(char), amount, fo );
+	      if (written != amount)
+		{
+		  result = false;
+		}
+	    }
+	} // when amount read is < BUFSZ, copy is done
+      while ((result) && (amount == BUFSZ));
+      fclose(fi);
+      fclose(fo);
+    }
+  delete [] buf;
+  return(result);
+}
+
+// ------------------------------------------------------------
+
+static
+void do_dasm_test (const std::string& table_file, dasm_result_c& result)
+{
+
+  table_reader_c reader (table_file.c_str ());
   char place [sizeof(DISASM)];
 
   for (size_t i=0; i<reader.items (); i++)
@@ -36,16 +104,19 @@ static void do_disasm_test (const char* table_file, dasm_result_c& result)
       result.add (dasm, ti, len, e);
     }
 }
-// ----------------------------------------------------------
+
+// ===================================================================
+
 void show_zip_content (const char* zipfile)
 {
   zip_iarchive_c zip (zipfile);
   for (zip_iarchive_c::iterator_t i = zip.begin (); i != zip.end (); i++)
     {
-      std::cout << i->name () << std::endl;
+      std::cout << i->name () << ":" << i->comment () << std::endl;
     }
 }
 // ----------------------------------------------------------
+#if BUGBUG
 int main (int argc, char* argv [])
 {
   if (argc != 2)
@@ -69,4 +140,97 @@ int main (int argc, char* argv [])
   return 1;
 #endif
   show_zip_content (table_file);
+}
+#endif
+// ----------------------------------------------------------
+static
+void do_dry_run (const std::string& table_file)
+{
+  std::cout << "Loading table from " << table_file << std::endl;
+  dasm_result_c result ("dry run", "");
+  do_dasm_test (table_file, result);
+  std::cout << "Total time:" << result.total_time () << " msec" << std::endl
+	    << "Avg time: " << result.avg_time () << " msec" << std::endl;
+}
+// ----------------------------------------------------------
+static
+void do_run_test (const std::string& table_file, const std::string& result_file,
+		  const std::string& name,       const std::string& comment)
+{
+  std::cout << "Loading table from " << table_file << std::endl;
+  dasm_result_c dasm_results (name.c_str (), comment.c_str ());
+  std::cout << "Running test" << std::endl;
+  do_dasm_test (table_file, dasm_results);
+
+  std::string temp = get_temp_path ();
+#if !defined (WIN32)
+  temp += "/";
+#else
+  temp += "\\";
+#endif
+  temp += name;
+  std::ofstream ofs;
+  ofs.open (temp.c_str ());
+  std::cout << "Saving temporary file " << temp << std::endl;
+  dasm_to_sexp (ofs, dasm_results);
+  ofs.close ();
+  std::cout << "Updating ZIP database" << std::endl;
+  zip_mode_t zip_mode = eAPPEND;
+
+  std::string tmp_zip;
+
+  if (!file_exists (result_file))
+    {
+      std::cout << "Database " << result_file << " is missing -- creating" << std::endl;
+      zip_mode = eCREATE;
+    }
+  else
+    {
+      std::cout << "Database " << result_file << " exists -- updating" << std::endl;
+      tmp_zip = temp + ".ZZZ";
+      std::cout << "Storing copy of the old Database" << std::endl;
+      copy_file (result_file, tmp_zip);
+    }
+  try
+    {
+      zip_oarchive_c zip (result_file.c_str (), zip_mode);
+      zip.put (temp, comment);
+    }
+  catch (std::exception& e)
+    {
+      std::cerr << "Error in ZIP module: " << e.what () << std::endl;
+      unlink (result_file.c_str ());
+      if (!tmp_zip.empty ())
+	{
+	  copy_file (tmp_zip, result_file);
+	}
+    }
+  std::cout << "Removing temporary file" << temp << std::endl;
+  unlink (temp.c_str ());
+  
+}
+// ----------------------------------------------------------
+int main (int argc, char* argv [])
+{
+  try
+    {
+      cmd_line_c cl (argc, argv);
+      if (cl.dry_run ())
+	{
+	  do_dry_run (cl.table_file ());
+	  return 0;
+	}
+      if (cl.run_test ())
+	{
+	  do_run_test (cl.table_file (), cl.result_file (),
+		       cl.name ()      , cl.comment ());
+	  return 0;
+	}
+    }
+  catch (std::exception& e)
+    {
+      std::cerr <<"Error: " << e.what () << std::endl;
+      return 1;
+    }
+  return 0;
 }
