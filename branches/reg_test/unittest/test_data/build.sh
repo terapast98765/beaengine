64bb8a3c784893132f2dc25a6940bd1374e3e914
igor.gutnik@gmail.com <igor.gutnik@gmail.com@e67b0f8a-f93e-11de-ba51-6b4196a2b81e>
2010-09-27 14:53:35 +0000

    unittest data organized and checked

--- branches/reg_test/unittest/test_data/build.sh
+++ branches/reg_test/unittest/test_data/build.sh
@@ -2,17 +2,30 @@
 pth=`pwd`
 dasm_exec=${pth}/../../bin/Linux.gnu.Debug/ndisasm
 filter_exec=${pth}/../../bin/Linux.gnu.Debug/filter_table
+
 for i in 16 32 64; do
     for j in 16 32 64; do
 	oname="t_${i}_${j}"
 	nasm -fbin -o ${oname}.bin ${oname}.asm
 	${dasm_exec} -b ${j} ${oname}.bin > dasm.temp
-	cat dasm.temp | awk '{$1 = ""; $2 = ""; print}' | sed -e 's/^ //g' > ${oname}.i
 	cat dasm.temp | awk '{$1 = ""; print}' | sed -e 's/^ //g' > ${oname}.d
 	rm dasm.temp
 	${filter_exec} ${oname}.d ${j} > table_${i}_${j}.dasm
 	rm ${oname}.d
-	rm ${oname}.i
 	rm ${oname}.bin
     done
 done
+
+touch temp_table.dasm
+for i in 16 32 64; do
+    for j in 16 32 64; do
+	oname="table_${i}_${j}.dasm"
+	cat $oname >> temp_table.dasm
+	rm ${oname}
+    done
+done
+
+${filter_exec} temp_table.dasm > table.dasm
+rm temp_table.dasm
+
+cat table.dasm | sed -e 's/^.*)//g' | awk '{print $1}' | sort -u | uniq > inst.txt
